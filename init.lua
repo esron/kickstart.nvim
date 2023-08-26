c3ebd566b6c0ec376d7a776a114921252150b906
Esron Silva <esron.dtamar@gmail.com>
2023-08-26 20:47:35 -0300

    Indent using spaces

--- init.lua
+++ init.lua
@@ -1006,6 +1006,8 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- Indent with spaces
+vim.o.expandtab = true
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
