5f614a452b76c0ed169e7e60d92fcd0eb80326cb
Esron Silva <essilva@redhat.com>
2023-10-09 11:15:33 -0300

    Upgrade ibl

--- init.lua
+++ init.lua
@@ -1096,11 +1096,7 @@ vim.opt.list = true
 vim.opt.listchars:append "space:."
 vim.opt.listchars:append "eol:↴"
 
-require("indent_blankline").setup {
-  show_end_of_line = true,
-  show_current_context = true,
-  show_current_context_start = true,
-}
+require("ibl").setup()
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
