fd552796bc29f5abd1b56ae85cd25b358593a3fb
Esron Silva <essilva@essilva-thinkpadp16vgen1.rmtbr.csb>
2024-02-27 14:51:51 -0300

    Configure tabnine-nvim

--- lua/custom/plugins/tabnine.lua
+++ lua/custom/plugins/tabnine.lua
@@ -1,4 +1,14 @@
 return {
-	"codota/tabnine-nvim",
-	build = "./dl_binaries.sh"
+  'codota/tabnine-nvim',
+  config = {
+    require('tabnine').setup {
+      disable_auto_comment = true,
+      accept_keymap = '<Tab>',
+      dismiss_keymap = '<C-]>',
+      debounce_ms = 800,
+      suggestion_color = { gui = '#808080', cterm = 244 },
+      exclude_filetypes = { 'TelescopePrompt', 'NvimTree' },
+      log_file_path = nil, -- absolute path to Tabnine log file
+    },
+  },
 }
