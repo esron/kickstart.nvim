80147ced9eb5b696f78489f531f7576eb962bcc6
Esron Silva <essilva@essilva-thinkpadp16vgen1.rmtbr.csb>
2024-02-28 17:58:14 -0300

    Config autoformat

--- init.lua
+++ init.lua
@@ -769,7 +769,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'ruff' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
@@ -1296,11 +1296,6 @@ local on_attach = function(_, bufnr)
   nmap('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format() { async = true }
-  end, { desc = 'Format current buffer with LSP' })
 end
 
 -- document existing key chains
