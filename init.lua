839f9d25660f2ff395de96f4740f9cfc9f094867
Esron Silva <essilva@essilva-thinkpadp16vgen1.rmtbr.csb>
2024-09-05 14:02:36 -0300

    Add file tree navigation and line moving

--- init.lua
+++ init.lua
@@ -174,6 +174,13 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Open file tree
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Move lines up and donw
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
